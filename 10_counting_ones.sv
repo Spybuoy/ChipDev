2e89e5cf0c34f4f5366ce3f8eb9c197256f7ce5d
Spybuoy <paladi.chakradhar01@gmail.com>
2025-04-14 19:17:55 -0700

    added pointers

--- 10_counting_ones.sv
+++ 10_counting_ones.sv
@@ -9,10 +9,13 @@ integer i;
 logic [$clog2(DATA_WIDTH):0] temp;
 
 always@(*) begin
+ // Assign initial value to temp as 0
+ // make sure not to manipulate \output\
   temp = 0;
 
-  for(i=0;i<DATA_WIDTH;i+=1) begin
-    // if(din[i]) temp = temp + 1'b1;
+  for(i=0;i<DATA_WIDTH;i+=1) begin // watchout for for-loop end
+    // if(din[i]) temp = temp + 1'b1; 
+    // Can use this += is not supported
     temp += din[i];
   end
 
