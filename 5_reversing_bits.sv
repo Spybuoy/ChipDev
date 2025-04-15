ce33618a2cc9d5145a34d172d984aa27fa6c4327
Spybuoy <paladi.chakradhar01@gmail.com>
2025-04-14 19:42:59 -0700

    Added pointer

--- 5_reversing_bits.sv
+++ 5_reversing_bits.sv
@@ -4,7 +4,7 @@ module model #(parameter
   input  [DATA_WIDTH-1:0] din,
   output logic [DATA_WIDTH-1:0] dout
 );
-integer i;
+integer i; // int is 2-type and integer is 4-type
 always@(*) begin
   for(i=0;i<DATA_WIDTH;i++) begin
     dout[i] = din[DATA_WIDTH-1-i];
