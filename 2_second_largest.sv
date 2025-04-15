f67c71cdc624baba11d67d6aba03fc569cf9298f
Spybuoy <paladi.chakradhar01@gmail.com>
2025-04-14 19:15:17 -0700

    Added pointers

--- 2_second_largest.sv
+++ 2_second_largest.sv
@@ -9,15 +9,23 @@ module model #(parameter
 reg [DATA_WIDTH-1:0] largest, second;
 
 always@(posedge clk) begin
-  if(~resetn) begin
+  if(~resetn) begin // When reset is asserted, turn both to zero
     largest<=0;
     second<=0;
   end
   else if (din>largest && din>second) begin
+	/*
+	When reset is deasserted
+	and din is larger than both of them
+	*/
     largest<=din;
     second<=largest;
   end
   else if(din>second) begin
+	/*
+	When reset is deasserted
+	and din is between largest and second 
+	*/
     second<=din;
   end
 end
