d03b6db17e665edf1a54415a2c5fcded95ede74b
Spybuoy <chakri@chakri.localdomain>
2025-12-08 06:18:19 -0700

    gray = (bin>>1)^bin

--- 4_gray_code_counter.sv
+++ 4_gray_code_counter.sv
@@ -1,29 +1,21 @@
-module model #(parameter
-  DATA_WIDTH = 4
+module model #(
+    parameter DATA_WIDTH = 4
 ) (
-  input clk,
-  input resetn,
-  output logic [DATA_WIDTH-1:0] out
+    input clk,
+    input resetn,
+    output logic [DATA_WIDTH-1:0] out
 );
-integer i;
-logic [DATA_WIDTH-1:0] q, temp;
 
-always@(posedge clk) begin // counter
-  if(~resetn) begin
-    temp<=0;
-    q<=1; // this is delayed assignment, need q to be 1 to avoid 0,0,1...
-  end
-  else begin
-    q<=q+1;
+  logic [DATA_WIDTH-1:0] temp;
 
-    for(i=0;i<DATA_WIDTH-1;i++) begin
-      temp[i] <= q[i+1] ^ q[i]; // abc, b xor c 
+
+  always @(posedge clk) begin // counter
+    if (~resetn) begin
+      temp <= 0;
+    end else begin
+      temp <= temp + 1'b1;
     end
-
-    temp[DATA_WIDTH-1] <= q[DATA_WIDTH-1]; // a
   end
-end
-assign out = temp; // assign logic/reg to output
-// avoid changes to output variables
 
+  assign out = (temp >> 1) ^ temp; // binary to gray
 endmodule
